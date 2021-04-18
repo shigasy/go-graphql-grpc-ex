0e593ff30c48f64386620094bc2a6c0fcc0e2418
shigasy <kxx555u.u@gmail.com>
2021-04-19 02:40:09 +0900

    resolverでの処理を行うコードが冗長になってしまうことを避けるためにarticleサービスのclientに処理を書いた

--- article/client/client.go
+++ article/client/client.go
@@ -1,7 +1,11 @@
 package client
 
 import (
+	"context"
+	"io"
+
 	"github.com/shigasy/go-graphql-grpc-ex/article/pb"
+	"github.com/shigasy/go-graphql-grpc-ex/graph/model"
 	"google.golang.org/grpc"
 )
 
@@ -23,4 +27,91 @@ func NewClient(url string) (*Client, error) {
 
 	// articleサービスのclientを返す
 	return &Client{conn, c}, nil
+}
+
+func (c *Client) CreateArticle(ctx context.Context, input *pb.ArticleInput) (*model.Article, error) {
+	res, err := c.Service.CreateArticle(
+		ctx,
+		&pb.CreateArticleRequest{ArticleInput: input},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &model.Article{
+		ID:      int(res.Article.Id),
+		Author:  res.Article.Author,
+		Title:   res.Article.Title,
+		Content: res.Article.Content,
+	}, nil
+
+}
+
+func (c *Client) ReadArticle(ctx context.Context, id int64) (*model.Article, error) {
+	// READ処理のレスポンスを受け取る
+	res, err := c.Service.ReadArticle(ctx, &pb.ReadArticleRequest{Id: id})
+	if err != nil {
+		return nil, err
+	}
+
+	// GraphQLサービスで扱える形にしてREADしたArticleを返す
+	return &model.Article{
+		ID:      int(res.Article.Id),
+		Author:  res.Article.Author,
+		Title:   res.Article.Title,
+		Content: res.Article.Content,
+	}, nil
+}
+
+func (c *Client) UpdateArticle(ctx context.Context, id int64, input *pb.ArticleInput) (*model.Article, error) {
+	//UPDATE処理のレスポンスを受け取る
+	res, err := c.Service.UpdateArticle(ctx, &pb.UpdateArticleRequest{Id: id, ArticleInput: input})
+	if err != nil {
+		return nil, err
+	}
+
+	// GraphQLサービスで扱える形にしてUPDATEしたArticleを返す
+	return &model.Article{
+		ID:      int(res.Article.Id),
+		Author:  res.Article.Author,
+		Title:   res.Article.Title,
+		Content: res.Article.Content,
+	}, nil
+}
+
+func (c *Client) DeleteArticle(ctx context.Context, id int64) (int64, error) {
+	// DELETE処理のレスポンスを受け取る
+	res, err := c.Service.DeleteArticle(ctx, &pb.DeleteArticleRequest{Id: id})
+	if err != nil {
+		return 0, err
+	}
+	// DELETEしたArticleのIDを返す
+	return res.Id, nil
+}
+
+func (c *Client) ListArticle(ctx context.Context) ([]*model.Article, error) {
+	// 全取得処理のレスポンスを受け取る
+	res, err := c.Service.ListArticle(ctx, &pb.ListArticleRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	// GraphQLサービスで扱える形にして全取得した記事をスライスとして返す
+	var articles []*model.Article
+	for {
+		r, err := res.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		articles = append(articles, &model.Article{
+			ID:      int(r.Article.Id),
+			Author:  r.Article.Author,
+			Title:   r.Article.Title,
+			Content: r.Article.Content,
+		})
+	}
+	return articles, nil
 }
