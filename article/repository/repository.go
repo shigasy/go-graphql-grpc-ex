635e41bdeccbce3c985fdcc4940a4bcf161c6612
shigasy <kxx555u.u@gmail.com>
2021-04-19 00:39:20 +0900

    sqlite3を入れて、repository層からDBに問い合わせる関数を追加

---
+++ article/repository/repository.go
@@ -0,0 +1,118 @@
+package repository
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Repository interface {
+	InsertArticle(ctx context.Context, input *pb.ArticleInput) (int64, error)
+	SelectArticleByID(ctx context.Context, id int64) (*pb.Article, error)
+	UpdateArticle(ctx context.Context, id int64, input *pb.ArticleInput) error
+	DeleteArticle(ctx context.Context, id int64) error
+	SelectAllArticles() (*sql.Rows, error)
+}
+
+type sqliteRepo struct {
+	db *sql.DB
+}
+
+func NewsqliteRepo() (Repository, error) {
+	db, err := sql.Open("sqlite3", "./article/article.sql")
+	if err != nil {
+		return nil, err
+	}
+
+	// articlesテーブルを作成
+	cmd := `CREATE TABLE IF NOT EXISTS articles(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			author STRING,
+			title STRING,
+			content STRING)`
+
+	_, err = db.Exec(cmd)
+	if err != nil {
+		return nil, err
+	}
+	return &sqliteRepo{db}, nil
+}
+
+func (r *sqliteRepo) InsertArticle(ctx context.Context, input *pb.ArticleInput) (int64, error) {
+	// DBに記事をINERT
+	// Inputの内容(Author, Title, Content)をarticlesテーブルにINSERT
+	cmd := "INSERT INTO articles(author, title, content) VALUES (?, ?, ?)"
+	result, err := r.db.Exec(cmd, input.Author, input.Title, input.Content)
+	if err != nil {
+		return 0, err
+	}
+
+	// INSERTした記事のIDを取得
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// INSERTした記事のIDを返す
+	return id, nil
+}
+
+func (r *sqliteRepo) SelectArticleByID(ctx context.Context, id int64) (*pb.Article, error) {
+	// DBからIDに基づいて記事をSELECT
+
+	// 該当IDの記事をSELECT
+	cmd := "SELECT * FROM articles WHERE id = ?"
+	row := r.db.QueryRow(cmd, id)
+	var a pb.Article
+
+	// SELECTした記事の内容を読み取る
+	err := row.Scan(&a.Id, &a.Author, &a.Title, &a.Content)
+	if err != nil {
+		return nil, err
+	}
+
+	// SELECTした記事を返す
+	return &pb.Article{
+		Id:      a.Id,
+		Author:  a.Author,
+		Title:   a.Title,
+		Content: a.Content,
+	}, nil
+}
+
+func (r *sqliteRepo) UpdateArticle(ctx context.Context, id int64, input *pb.ArticleInput) error {
+	//　DB内の記事をUPDATE
+	// 該当IDのAuthor, Title, ContentをUPDATE
+	cmd := "UPDATE articles SET author = ?, title = ?, content = ? WHERE id = ?"
+	_, err := r.db.Exec(cmd, input.Author, input.Title, input.Content, id)
+	if err != nil {
+		return err
+	}
+
+	// errorがなければ返り値なし
+	return nil
+}
+
+func (r *sqliteRepo) DeleteArticle(ctx context.Context, id int64) error {
+	// DB内の記事をDELETE
+	// 該当IDの記事をDELETE
+	cmd := "DELETE FROM articles WHERE id = ?"
+	_, err := r.db.Exec(cmd, id)
+	if err != nil {
+		return err
+	}
+
+	// errorがなければ返り値なし
+	return nil
+}
+
+func (r *sqliteRepo) SelectAllArticles() (*sql.Rows, error) {
+	// articlesテーブルの記事を全取得
+	// articlesテーブルの記事を全取得
+	cmd := "SELECT * FROM articles"
+	rows, err := r.db.Query(cmd)
+	if err != nil {
+		return nil, err
+	}
+
+	// 全取得した記事を*sql.Rowsの形で返す
+	return rows, nil
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
 	golang.org/x/net v0.0.0-20210415231046-e915ea6b2b7d // indirect
 	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
 	google.golang.org/genproto v0.0.0-20210416161957-9910b6c460de // indirect
